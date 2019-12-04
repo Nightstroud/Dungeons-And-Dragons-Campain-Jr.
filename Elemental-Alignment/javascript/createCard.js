3dd9a8093776c250f2aa99a24b0ba119d7cac184
Nightstroud <Nightstroud@yahoo.com>
2019-12-04 10:43:49 -0700

    Adjusting the Elemental Alignment Page

---
+++ Elemental-Alignment/javascript/createCard.js
@@ -0,0 +1,47 @@
+let selectedAlignment = null;
+class Card {
+    constructor() {
+        this.cards = JSON.parse(localStorage.getItem("CARDS"));
+        if (!this.cards) {
+            this.cards = [];
+        }
+    }
+
+    createCard(name, url) {
+        let card = `
+        <div class="card" onclick="setSelection('${name}')">
+                <img src="../resources/${url}.png">
+                <p>${name}</p>
+            </div>`;
+        this.cards.push(card);
+        this.displayCards();
+    }
+
+    displayCards() {
+        document.getElementById("cardContainer").innerHTML += "";
+        this.cards.forEach(card => {
+            document.getElementById("cardContainer").innerHTML += card;
+        });
+        localStorage.setItem('CARDS', JSON.stringify(this.cards));
+    }
+
+    deleteCard(index) {
+            this.cards.splice(index, 1);
+            this.displayCards();
+            return "Operation Completed, Reload the page to see your changes.";
+    }
+}
+let AlignmentCards = new Card();
+
+
+function GetCardDetails() {
+        document.getElementById("modalContainer").style.display = "flex";
+}
+
+function setSelection(selection) {
+    selectedAlignment = selection;
+    document.getElementById("container").style.display = 'none';
+    // createSkillStore();
+}
+
+AlignmentCards.displayCards();
